86bad3db5b04f956f64f568c9499153d6fbdda01
romantut1988 <romantut1988@mail.ru>
2022-08-15 10:45:51 +0300

    connecting plugins to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import {Home} from './pages/Home/Home';
+import {MonthStatistics} from'./pages/MonthStatistics/MonthStatistics';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <div className="App">
+    <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/month-statistics" exact component={MonthStatistics}/>
+    </Switch>
+  </div>;
 }
 
 export default App;
