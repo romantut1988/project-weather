d6ff8e684c51d41793bcfc5016ea4d422bf69c01
romantut1988 <romantut1988@mail.ru>
2022-08-16 22:55:19 +0300

    project layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import {Home} from './pages/Home/Home';
-import {MonthStatistics} from'./pages/MonthStatistics/MonthStatistics';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
+import { Header } from "./shared/Header/Header";
+import { Popup } from "./shared/Popup/Popup";
 
 function App() {
-  return <div className="App">
-    <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/month-statistics" exact component={MonthStatistics}/>
-    </Switch>
-  </div>;
+  return (
+    <div className="global-container">
+      {/* <Popup /> */}
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/month-statistics" exact component={MonthStatistics} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Days } from "./components/Days/Days";
+import { ThisDay } from "./components/ThisDay/ThisDay";
+import { ThisDayInfo } from "./components/ThisDayInfo/ThisDayInfo";
 
-import s from './Home.module.scss';
+import s from "./Home.module.scss";
 
-interface Props{}
+interface Props {}
 
 export const Home = (props: Props) => {
-    return <div className={s.home}>Home</div>;
-}
+  return (
+    <div className={s.home}>
+      <div className={s.wrapper}>
+        <ThisDay />
+        <ThisDayInfo />
+      </div>
+
+      <Days />
+    </div>
+  );
+};
